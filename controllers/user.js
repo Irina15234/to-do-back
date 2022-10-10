bbdaa5390ee8aa2187003eeea3667dfb20a5295d
Irina15234 <irina15234@yandex.ru>
2022-10-10 20:06:01 +0300

    get user info view

--- controllers/user.js
+++ controllers/user.js
@@ -3,8 +3,14 @@ const {getUserId} = require("../common/helpers");
 
 exports.getUser = function (req, res) {
     const userId = getUserId(req.headers.authorization);
-    db.query(`SELECT * FROM users WHERE ${userId}=users.id LIMIT 1`, function (err, data) {
+
+    const view = req.query.view;
+
+    db.query(`SELECT id, name, username, email, phone, photo FROM users WHERE ${userId}=users.id LIMIT 1`, function (err, data) {
         if (err) return console.log(err);
-        res.send({ id: data[0].id, name: data[0].name, photo: data[0].photo });
+        if (!data.length) return console.log('User not found');
+
+        const result = view === 'FULL' ? {...data[0]} : { id: data[0].id, name: data[0].name, photo: data[0].photo };
+        res.send(result);
     });
 };
